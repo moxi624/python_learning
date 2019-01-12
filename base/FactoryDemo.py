f1b695776fea11ec8d11aee980262da20bc421e9
陌溪_ <xzx19950624@qq.com>
2019-01-12 23:13:24 +0800

    增加python相关学习代码

--- base/FactoryDemo.py
+++ base/FactoryDemo.py
@@ -2,17 +2,13 @@
 
 
 class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-    def __init__(self, name):
-        self.name = name;
-
-    # def __init__(self, name, age):
-    #     self.name = name
-    #     self.age = age
-
-    def work(self):
+    def work(self, type_axe):
         print( self.name + "开始工作了")
-        axe = StoneAxe();
+        axe = Factory.create_axe(type_axe);
         axe.cut_tree();
 
 class Axe(object):
@@ -20,12 +16,35 @@ class Axe(object):
         print("正在砍树");
 
 class StoneAxe(Axe):
+    def __init__(self, name):
+        self.name = name;
     def cut_tree(self):
         print("使用石头做的斧头砍树");
 
 class SteeAxe(Axe):
+    def __init__(self, name):
+        self.name = name;
     def cut_tree(self):
         print("使用钢斧头砍树");
 
-p = Person('gfadf 男');
-p.work();
+class WaterAxe(Axe):
+    def __init__(self, name):
+        self.name = name;
+    def cut_tree(self):
+        print("使用水砍树");
+
+class Factory(object):
+
+    @staticmethod
+    def create_axe(type):
+        if type == "sthone":
+            return StoneAxe("花岗岩斧头");
+        elif type == "steel":
+            return SteeAxe("加爵斧头");
+        elif type == "water":
+            return WaterAxe("水斧头");
+        else:
+            print("传入的类型不同");
+
+p = Person("张三", 18);
+p.work("sthone");
